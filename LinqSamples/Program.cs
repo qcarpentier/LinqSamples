46decf7307fe41a6b31bd8e9e3bea17b7777df93
Quentin Carpentier <quentin.carpentier@outlook.be>
2017-10-17 15:48:58 +0200

    Introducing linq for files tracking

--- LinqSamples/Program.cs
+++ LinqSamples/Program.cs
@@ -1,15 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace LinqSamples
+namespace FileTracker
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string path = @"C:\windows";
+            ShowLargeFilesWithoutUsingLinq(path);
+            Console.WriteLine("***");
+            ShowLargeFilesWithUsingLinq(path);
+            Console.WriteLine("***");
+            ShowLargeFilesWithUsingLinqSyntax(path);
+        }
+
+        private static void ShowLargeFilesWithUsingLinqSyntax(string path)
+        {
+            var query = new DirectoryInfo(path).GetFiles()
+                            .OrderByDescending(f => f.Length)
+                            .Take(5);
+
+            foreach (var file in query)
+            {
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            }
+        }
+
+        private static void ShowLargeFilesWithUsingLinq(string path)
+        {
+            var query = from file in new DirectoryInfo(path).GetFiles()
+                        orderby file.Length descending
+                        select file;
+
+            foreach (var file in query.Take(5))
+            {
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            }
+        }
+
+        private static void ShowLargeFilesWithoutUsingLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            // Sort the files
+            Array.Sort(files, new FileInfoComparer());
+
+            for (int i = 0; i < 5; i++)
+            {
+                FileInfo file = files[i];
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            }
+        }
+    }
+
+    public class FileInfoComparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
         }
     }
 }
