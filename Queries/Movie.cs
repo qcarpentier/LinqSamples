aa53af5a7be0f3652774ad85c9a7020d06762d5c
Carpentier Quentin <quentcarp@hotmail.com>
2017-10-18 22:47:50 +0200

    Filter custom Linq method

--- Queries/Movie.cs
+++ Queries/Movie.cs
@@ -10,6 +10,17 @@ namespace Queries
     {
         public string Title { get; set; }
         public float Rating { get; set; }
-        public int Year { get; set; }
+        public int Year {
+            get
+            {
+                Console.WriteLine($"Returning {_year} for {Title}");
+                return _year;
+            }
+            set
+            {
+                _year = value;
+            }
+        }
+        private int _year;
     }
 }
