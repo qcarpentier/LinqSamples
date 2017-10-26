35bc2fdcdfd4b0860c679940ace08fa55ceb1be1
Carpentier Quentin <quentcarp@hotmail.com>
2017-10-26 15:42:58 +0200

    Car project added with 1200 lines from csv file + log the names with LINQ

---
+++ Cars/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = ProcessFile("fuel.csv");
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.Name);
+            }
+        }
+
+        private static List<Car> ProcessFile(string path)
+        {
+            return
+                File.ReadAllLines(path)
+                    .Skip(1)
+                    .Where(line => line.Length > 1)
+                    // Projection
+                    .Select(Car.ParseFromCsv)
+                    // Concrete DS
+                    .ToList();
+
+            // Query syntax
+            //var query = 
+            //    from line in File.ReadAllLines(path).Skip(1)
+            //    where line.Length > 1
+            //    select Car.ParseFromCsv(line);
+
+            //return query.ToList();
+        }
+    }
+
+}
